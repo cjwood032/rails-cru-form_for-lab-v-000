cdb0f5e33289dbd1893b78f2064fc03ada60b9de
Christopher Wood <cjwood032@gmail.com>
2018-08-05 19:27:46 +0000

    automatically backed up by learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,8 +1,12 @@
 class ArtistsController < ApplicationController
   def new
+    @genre= Genre.new
   end
 
   def create
+    @genre = Genre.new(genre_params)
+    @genre.save
+    redirect_to genre_path(@genre)    
   end
 
   def show

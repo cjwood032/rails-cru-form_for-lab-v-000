556ecf6dba1ed1048d311cc059e722d424a0f175
Christopher Wood <cjwood032@gmail.com>
2018-08-05 19:30:27 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,17 @@
 class ArtistsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  
   def new
+    @song=Song.new
   end
 
   def create
+    @song = Song.new(song_params)
+    @song.save
+    redirect_to song_path(@song)
   end
 
   def show
